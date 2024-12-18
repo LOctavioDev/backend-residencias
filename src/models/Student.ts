cc0bdb90213aaa2f7fbab5201133ad07aa51b421
Luis Octavio <octaviodevtech@gmail.com>
2024-12-17 18:08:40 -0600

    ADD: Final

--- src/models/Student.ts
+++ src/models/Student.ts
@@ -38,7 +38,7 @@ interface IStudent extends Document {
     company_phone: string;
     fax: string;
     company_email: string;
-    salary: number;
+    salary: string;
     name: string;
     location: {
       city: string;
@@ -66,81 +66,80 @@ interface IStudent extends Document {
   profile: string;
   contact_source: string;
   studentAt: string;
+  updatedAt: Date;
 }
 
 // ! CREATE THE SCHEMA BASED ON THE INTERFACE
 
-const StudentSchema: Schema = new Schema(
-  {
-    control_number: { type: String, required: true, unique: true },
-    career: { type: String, required: true },
-    name: {
-      first: { type: String, required: true },
-      last: { type: String, required: true },
-      middle: String,
-    },
-    generation: {
-      startDate: { type: Number, required: true },
-      endDate: { type: Number, required: true },
-    },
-    email: { type: String, required: true, unique: true },
-    discharge_date: { type: Number, required: false },
-    activity: {
-      activities: { type: [String], required: true },
-    },
-    birthdate: { type: Number, required: true },
-    curp: { type: String, required: true, unique: true },
-    marital_status: { type: String, required: true },
-    gender: { type: String, required: true },
-    home_address: { type: String, required: true },
-    cp: { type: String, required: true },
-    student_city: { type: String, required: true },
-    student_municipality: { type: String, required: true },
-    student_state: { type: String, required: true },
-    phone: { type: String, required: true },
-    certificate: { type: String, required: true },
-    graduation_date: { type: Number, required: true },
-    graduation_option: { type: String, required: true },
-    post_graduation: { type: String, required: true },
-    company: {
-      boss_name: { type: String, required: true },
-      boss_position: { type: String, required: true },
-      company_address: { type: String, required: true },
-      company_cp: { type: String, required: true },
-      company_phone: { type: String, required: true },
-      fax: { type: String, required: false },
-      company_email: { type: String, required: true },
-      salary: { type: Number, required: true },
-      name: { type: String, required: true },
-      location: {
-        city: { type: String, required: true },
-        municipality: { type: String, required: true },
-        state: { type: String, required: true },
-      },
-      position: { type: String, required: true },
-      years_in_position: { type: Number, required: false },
-      hierarchical_level: { type: String, required: true },
-      startDateC: { type: Number, required: false },
-      endDateC: { type: Number, required: false },
-    },
-    companyHistory: [
-      {
-        name: { type: String, required: false },
-        startDateH: { type: Number, required: false },
-        endDateH: { type: Number, required: false },
-      },
-    ],
-    working_condition: {
-      type: { type: String, required: true },
-    },
-    sector: { type: String, required: true },
-    institution: { type: String, required: true },
-    profile: { type: String, required: true },
-    contact_source: { type: String, required: true },
-    studentAt: { type: String, required: true },
+const StudentSchema: Schema = new Schema({
+  control_number: { type: String, required: true, unique: true },
+  career: { type: String, required: true },
+  name: {
+    first: { type: String, required: true },
+    last: { type: String, required: true },
+    middle: String,
   },
-  { timestamps: true }
-);
+  generation: {
+    startDate: { type: Number, required: true },
+    endDate: { type: Number, required: true },
+  },
+  email: { type: String, required: true, unique: true },
+  discharge_date: { type: Number, required: false },
+  activity: {
+    activities: { type: [String], required: true },
+  },
+  birthdate: { type: Number, required: true },
+  curp: { type: String, required: true, unique: true },
+  marital_status: { type: String, required: true },
+  gender: { type: String, required: true },
+  home_address: { type: String, required: true },
+  cp: { type: String, required: true },
+  student_city: { type: String, required: true },
+  student_municipality: { type: String, required: true },
+  student_state: { type: String, required: true },
+  phone: { type: String, required: true },
+  certificate: { type: String, required: true },
+  graduation_date: { type: Number, required: true },
+  graduation_option: { type: String, required: true },
+  post_graduation: { type: String, required: true },
+  company: {
+    boss_name: { type: String, required: true },
+    boss_position: { type: String, required: true },
+    company_address: { type: String, required: true },
+    company_cp: { type: String, required: true },
+    company_phone: { type: String, required: true },
+    fax: { type: String, required: false },
+    company_email: { type: String, required: true },
+    salary: { type: String, required: true },
+    name: { type: String, required: true },
+    location: {
+      city: { type: String, required: true },
+      municipality: { type: String, required: true },
+      state: { type: String, required: true },
+    },
+    position: { type: String, required: true },
+    years_in_position: { type: Number, required: false },
+    hierarchical_level: { type: String, required: true },
+    startDateC: { type: Number, required: false },
+    endDateC: { type: Number, required: false },
+  },
+  companyHistory: [
+    {
+      name: { type: String, required: false },
+      startDateH: { type: Number, required: false },
+      endDateH: { type: Number, required: false },
+    },
+  ],
+  working_condition: {
+    type: { type: String, required: true },
+  },
+  sector: { type: String, required: true },
+  institution: { type: String, required: true },
+  profile: { type: String, required: true },
+  contact_source: { type: String, required: true },
+  studentAt: { type: String, required: true },
+  updatedAt: { type: Date, required: true },
+});
 
 const Student = mongoose.model<IStudent>('Student', StudentSchema);
 
